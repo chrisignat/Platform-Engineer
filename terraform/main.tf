c32cd217acbb65a2b450d790d4a6f917f74f4290
Chris Ignatiadis <43823317+chrisignat@users.noreply.github.com>
2026-04-17 18:40:07 +0300

    Patch

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,15 +7,10 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    s3 = "http://localhost:4566" # Χρησιμοποίησε την IP/URL που βλέπει το PC σου
+    s3 = "http://localhost:4566"
   }
 }
 
 resource "aws_s3_bucket" "velero_bucket" {
   bucket = "velero-backups"
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.velero_bucket.id
-  acl    = "private"
 }
