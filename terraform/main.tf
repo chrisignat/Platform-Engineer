917cdf795f5fbccff89c87e0c15e3bc908636296
Chris Ignatiadis <43823317+chrisignat@users.noreply.github.com>
2026-04-17 18:19:32 +0300

    Velero

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,21 @@
 provider "aws" {
-  region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
+  region                      = "us-east-1"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-    ec2 = "http://127.0.0.1:4566" # Χρησιμοποιούμε το Ingress που φτιάξαμε
+    s3 = "http://localhost:4566" # Χρησιμοποίησε την IP/URL που βλέπει το PC σου
   }
 }
 
-# 1. Δημιουργία ενός Security Group (για να ανοίξουμε την πόρτα 80)
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  description = "Allow HTTP inbound traffic"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_s3_bucket" "velero_bucket" {
+  bucket = "velero-backups"
 }
 
-# 2. Δημιουργία της EC2 Instance
-resource "aws_instance" "web_server" {
-  ami           = "ami-00000000" # Στο LocalStack τα AMI IDs είναι εικονικά
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_web.id]
-
-  tags = {
-    Name = "MyManualEC2"
-  }
-}
-
-# 3. Output για να δούμε το ID της
-output "instance_id" {
-  value = aws_instance.web_server.id
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.velero_bucket.id
+  acl    = "private"
 }
