e875a3566e9655b185b46d8bd5290fdeb18184da
Chris Ignatiadis <43823317+chrisignat@users.noreply.github.com>
2026-05-02 19:42:27 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,10 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
+  s3_use_path_style           = true 
+
   endpoints {
-    s3 = "http://localhost:4566"
+    s3 = "http://localstack.local" 
   }
 }
 
